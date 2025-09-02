ead279bd5988cd2df9ecb5385830db7ee533f9ca
Julian Moore <julian.moore7970@gmail.com>
2025-09-02 02:28:24 -0500

    Created the home screen, with no router.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,56 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import {onMounted, ref} from 'vue'
+import ButtonCard from './components/ButtonCard.vue';
+const study_img = "/public/why-study.png"
+const study_button = "Why you should study"
+
+const career_img = "/public/careers.jpg"
+const careers_button = "Possible Careers"
+
+const cs_life_img = "cs_major.jpg"
+const cs_life_button = "Life of a CS major"
+
+const learn_more_img ="learn_more.jpg"
+const learn_more_button ="How to explore and learn more"
+
+const prepare_img ="prepare.jpg"
+const prepare_button ="How to prepare"
+
+const cool_facts_img = "cool_facts.jpg"
+const cool_facts_button ="Cool CS facts"
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <h1 id= "home-screen-header"class = "center-text">The World of Computer Science</h1>
+  <div class = "grid">
+  <ButtonCard
+  :msg="study_button"
+  :img="study_img"/>
+  
+  <ButtonCard
+  :msg="careers_button"
+  :img="career_img"/>
+  
+  <ButtonCard
+  :msg="cs_life_button"
+  :img="cs_life_img"/>
+ 
+  <ButtonCard
+  :msg="learn_more_button"
+  :img="learn_more_img"/>
+  
+  <ButtonCard
+  :msg="prepare_button"
+  :img="prepare_img"/>
+  
+  <ButtonCard
+  :msg="cool_facts_button"
+  :img="cool_facts_img"/>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
+
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ src/components/ButtonCard.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {onMounted, ref} from 'vue'
+import {defineProps} from 'vue'
+const props = defineProps({
+  msg: String,
+  img: String
+})
+
+</script>
+
+<template>
+  <div class = "flex-column">
+  <p class = "button-card-image-container">
+      <img :src= img>
+    </p>
+    <div class = "flex-row bottom-half secondary-color">
+        <button>{{ props.msg }}</button>
+    </div>
+  </div>
+ 
+
+</template>
+
+<style scoped>
+
+</style>
