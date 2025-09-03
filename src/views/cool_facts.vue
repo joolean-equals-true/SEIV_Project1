d7895a9dc7e22e39ffcdfaf500ba4c8f0b1fda02
teamharriscali <teamharriscali@gmail.com>
2025-09-03 13:38:32 -0500

    Add styling for Cool Facts screen and update content

--- src/views/cool_facts.vue
+++ src/views/cool_facts.vue
@@ -5,13 +5,27 @@ const router = useRouter();
 </script>
 
 <template>
-  <div>
-    <h2>Sample Text</h2>
+  <div class = "cool-facts-header"> <h1>Cool CS Facts</h1> </div>
 
-    <router-link :to="{name: 'Home'}"><button>Home</button></router-link>
+  <div class="main-div">
+    <div class="text-color-text flex-column flex-row">
+      <ul>
+      <li>The first email was sent in 1971 by Ray Tomlinson</li>
+      <li>The first computer, ENIAC (1940s), weighed 30 tons and took up an entire room.</li>
+      <li>The first computer network was developed in the 1960s. It was called ARPNET (Advanced Research Projects Agency Network)</li>
+      <li>The term “computer bug” came from a moth that was stuck in a relay of the Harvard Mark II computer in 1947.</li>
+    </ul>
+    </div>
+    
+    <div class="text-color-text">
+      <p>Source: <a href="https://www.idtech.com/blog/computer-science-facts-collection-of-interesting-statistics" target="_blank">idtech.com</a></p>
+      <p>Source: <a href="https://www.dbvis.com/thetable/why-are-they-called-bugs/" target="_blank">dbvis.com</a></p>
+    </div>
   </div>
-  
- 
+
+  <div class="cool-facts-header">
+    <router-link :to="{name: 'Home'}"><button>Home</button></router-link>
+  </div> 
 
 </template>
 
