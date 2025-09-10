39cac2862afd293239b0a11d5cba08dec7aacb62
Julian Moore <julian.moore7970@gmail.com>
2025-09-10 17:33:53 -0500

    Finished Study

--- src/views/Divisions.vue
+++ src/views/Divisions.vue
@@ -9,7 +9,11 @@ const artificial_intelligence = "public/divisions_images/artificial-intelligence
 const artificial_intelligence_p = "In this division, you will learn about the tools behind AI, how it works, and the principles behind its functionality. This division will equip you with the tools you need to be a expert on the training, development, and deployment of AI models."
 const cybersecurity = "public/divisions_images/cybersecurity.jpg"
 const cybersecurity_p = "In the Cybersecurity division, you will focus on learning about how to defend our systems effectively. Courses such as Network and Cloud security will help prepare you to be at the forfront of an organization's defenses."
-
+const middle_img = "/public/divisions_images/middle_img.png"
+const gaming_img = "/public/divisions_images/gaming_img.png"
+const gaming_p = "In the gaming division, you will gain crucial concepts that are vital to game development: 3d modeling, world building, as well as training in the tools used to develop games, such as Unity, blender, and programming in C#."
+const business_img = "/public/divisions_images/business_img.png"
+const business_p = "In the Business division, you will learn about business principles to better understand the mindset of an organization while you learn CS and Software development principles, so that you can better market yourself to corporation."
 </script>
 
 <template>
@@ -31,12 +35,33 @@ const cybersecurity_p = "In the Cybersecurity division, you will focus on learni
       :img="cybersecurity"
       />
   </div>
-  <div class="flex-column"></div>
+  <div class="flex-column">
+    <h1 class = "center-text">Divisions</h1>
+    <p class = "image-container">
+      <img :src = "middle_img">
+    </p>
+  </div>
 
-  <div class="flex-column"></div>
+  <div id = "divisions-div1"class="flex-column">
+      <h2 class="accent-color-text">
+      Gaming
+    </h2>
+    <card
+      :msg="gaming_p"
+      :img="gaming_img"
+      />
+
+      <h2 class="accent-color-text">
+      Business
+    </h2>
+    <card
+      :msg="business_p"
+      :img="business_img"
+      />
+  </div>
   </div>
  <div class = flex-row>
-  <router-link :to="{name: 'Home'}"><button class = "home-button">Home</button></router-link>
+  <router-link :to="{name: 'Study'}"><button class = "home-button">Back</button></router-link>
  </div>
 
 
